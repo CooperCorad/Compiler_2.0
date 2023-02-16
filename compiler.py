f9a488ff094d7170752864353197fb998748b061
Cooper <u1310898@umail.utah.edu>
2023-02-15 21:17:35 -0700

    silly input fix before push

--- compiler.py
+++ compiler.py
@@ -5,10 +5,10 @@ import mytypechecker
 
 def main():
 
-    # flag = sys.argv[1]
-    # file_spec = sys.argv[2]
-    flag = '-t'
-    file_spec = 'test.jpl'
+    flag = sys.argv[1]
+    file_spec = sys.argv[2]
+    # flag = '-t'
+    # file_spec = 'test.jpl'
 
 
     if flag[0] != '-':
