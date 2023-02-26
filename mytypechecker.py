69919d3c8aa3c0f66caf3fd1b47053f30d77ec88
Cooper <u1310898@umail.utah.edu>
2023-02-26 13:13:51 -0700

    second attempt as passing tests, complex args??

--- mytypechecker.py
+++ mytypechecker.py
@@ -18,7 +18,7 @@ class TypeChecker:
 
     def type_check(self):
         global globaltable
-
+        # TODO command processing separation
         for expr in self.exprTree:
             expr.ty = self.type_of(expr, globaltable)
 
@@ -28,23 +28,7 @@ class TypeChecker:
         ret = ''
         for expr in self.exprTree:
             ret += expr.to_string() + '\n'
-
         return ret[:-1]
-
-    def addlval(self, lval: ArgLValue, table: SymbolTable, ty: ResolvedType):
-        if issubclass(ArgLValue, type(lval)):
-            if type(lval.variable) is VarArg:
-                name = lval.variable.variable.variable
-                table.addinfo(name, VariableInfo(ty))
-            elif isinstance(lval.variable, ArrayArgument):
-                name = lval.variable.var.variable
-                table.addinfo(name, VariableInfo(ty))
-
-    def addtuplelval(self, lval: TupleLValue, table: SymbolTable, ty: ResolvedType):
-        for i in range(len(lval.variables)):
-            if type(lval.variables[i]) is TupleLValue:
-                self.addtuplelval(lval.variables[i], table, ty.tys[i])
-            self.addlval(lval.variables[i], table, ty.tys[i])
 
     def type_of(self, baseexpr: Expr, table: SymbolTable):
 
@@ -86,17 +70,17 @@ class TypeChecker:
                       + lty.to_string() + ' ' + baseexpr.op + ' ' + rty.to_string() + ')'
                 raise TypeCheckerException(ret)
         elif type(baseexpr) is UnopExpr:
-            ty = self.type_of(baseexpr.expr, table)
-            if baseexpr.op == '!' and type(ty) is not BoolResolvedType:
-                ret = 'You cannot use boolean negation (!) on non bool types! (' + ty.to_string() + ')'
+            expty = self.type_of(baseexpr.expr, table)
+            if baseexpr.op == '!' and type(expty) is not BoolResolvedType:
+                ret = 'You cannot use boolean negation (!) on non bool types! (' + expty.to_string() + ')'
                 raise TypeCheckerException(ret)
-            elif baseexpr.op == '-' and (type(ty) is not FloatResolvedType and type(ty) is not IntResolvedType):
-                ret = 'You cannot use mathematical negation (-) on non mathematical types! (' + ty.to_string() + ')'
+            elif baseexpr.op == '-' and (type(expty) is not FloatResolvedType and type(expty) is not IntResolvedType):
+                ret = 'You cannot use mathematical negation (-) on non mathematical types! (' + expty.to_string() + ')'
                 raise TypeCheckerException(ret)
             else:
-                baseexpr.expr.ty = ty
-                baseexpr.ty = ty
-                return ty
+                baseexpr.expr.ty = expty
+                baseexpr.ty = expty
+                return expty
         elif type(baseexpr) is IfExpr:
             ifty = self.type_of(baseexpr.ifexp, table)
             if type(ifty) is not BoolResolvedType:
@@ -135,92 +119,118 @@ class TypeChecker:
             uniqueness = set(keeper)
             if len(uniqueness) > 1:
                 ret = 'You cannot have multiple types in an array literal declaration ('
-                for ty in tylist:
-                    ret += ty.to_string() + ', '
+                for expty in tylist:
+                    ret += expty.to_string() + ', '
                 raise TypeCheckerException(ret[:-2] + ')')
 
             return ArrayResolvedType(tylist[0], 1)    # TODO modular? for >1 rank arrays
         elif type(baseexpr) is TupleIndexExpr:
-            ty = self.type_of(baseexpr.varxpr, table)
-            size = len(ty.tys)
+            expty = self.type_of(baseexpr.varxpr, table)
+            size = len(expty.tys)
             if 0 < baseexpr.index > size - 1:
                 ret = 'You cannot access outside of the tuple literals bounds (' + str(size) + \
                       ' is incompatible with ' + str(baseexpr.index) + ')'
                 raise TypeCheckerException(ret)
-            baseexpr.varxpr.ty = ty
-            return ty.tys[baseexpr.index]
+            baseexpr.varxpr.ty = expty
+            return expty.tys[baseexpr.index]
         elif type(baseexpr) is ArrayIndexExpr:
             valuetys = self.type_of(baseexpr.expr, table)
             baseexpr.expr.ty = valuetys
-            if len(baseexpr.exprs) > valuetys.rank:
-                ret = 'You cannot access an array of rank ' + str(valuetys.rank) + ' at rank ' + str(len(baseexpr.exprs))
+            if len(baseexpr.exprs) != valuetys.rank:
+                ret = 'You cannot access an array of rank ' + str(valuetys.rank) + ' at access rank ' + str(len(baseexpr.exprs))
                 raise TypeCheckerException(ret)
 
             for val in baseexpr.exprs:
-                ty = self.type_of(val, table)
-                if type(ty) is not IntResolvedType:
-                    ret = 'You cannot access an array using a non integer! (' + ty.to_string() + ')'
+                expty = self.type_of(val, table)
+                if type(expty) is not IntResolvedType:
+                    ret = 'You cannot access an array using a non integer! (' + expty.to_string() + ')'
                     raise TypeCheckerException(ret)
-                val.ty = ty
+                val.ty = expty
 
             return valuetys.ty
         elif type(baseexpr) is ArrayLoopExpr:
+            if len(baseexpr.pairs) < 1:
+                ret = 'You cannot loop through a 0 rank array!'
+                raise TypeCheckerException(ret)
             arraytable = table.makechild()
+            for pair in baseexpr.pairs:
+                expty = self.type_of(pair[1], arraytable)
+                if type(expty) is not IntResolvedType:
+                    raise TypeCheckerException('You cannot iterate through arrays with non int increments ' + expty.to_string())
+                pair[1].ty = expty
             for pair in baseexpr.pairs:
                 if type(pair[0]) is Variable:
                     arraytable.addinfo(pair[0].variable, VariableInfo(IntResolvedType()))
-                ty = self.type_of(pair[1], arraytable)
-                if type(ty) is not IntResolvedType:
-                    raise TypeCheckerException('You cannot iterate through arrays with non int increments ' + ty.to_string())
-                pair[1].ty = ty
             loopty = self.type_of(baseexpr.expr, arraytable)
             baseexpr.expr.ty = loopty
             return ArrayResolvedType(loopty, len(baseexpr.pairs))
         elif type(baseexpr) is SumLoopExpr:
+            if len(baseexpr.pairs) < 1:
+                ret = 'You cannot loop through a 0 rank array!'
+                raise TypeCheckerException(ret)
             sumlooptable = table.makechild()
+            for pair in baseexpr.pairs:
+                expty = self.type_of(pair[1], table)
+                if type(expty) is not IntResolvedType:
+                    raise TypeCheckerException(
+                        'You cannot iterate through arrays with non int increments ' + expty.to_string())
+                pair[1].ty = expty
             for pair in baseexpr.pairs:
                 if type(pair[0]) is Variable:
                     sumlooptable.addinfo(pair[0].variable, VariableInfo(IntResolvedType()))
-
-                ty = self.type_of(pair[1], table)
-                if type(ty) is not IntResolvedType:
-                    raise TypeCheckerException(
-                        'You cannot iterate through arrays with non int increments ' + ty.to_string())
-                pair[1].ty = ty
             loopty = self.type_of(baseexpr.expr, sumlooptable)
+            if type(loopty) is not IntResolvedType and type(loopty) is not FloatResolvedType:
+                ret = 'You cannot sum on non mathematical types (Int or Float) you are using ' + loopty.to_string() + ' ' + baseexpr.to_string()
+                raise TypeCheckerException(ret)
+
             baseexpr.expr.ty = loopty
+
             return loopty
         # command checking
         elif type(baseexpr) is ShowCmd:
             showexpr = baseexpr.expr
-            ty = self.type_of(showexpr, table)
-            showexpr.ty = ty
-            return ty
+            expty = self.type_of(showexpr, table)
+            showexpr.ty = expty
+            return expty
         # TODO iffy on these?
         elif type(baseexpr) is AssertCmd:
             assertexpr = baseexpr.expr
-            ty = self.type_of(assertexpr, table)
-            if type(ty) is not BoolResolvedType:
-                raise TypeCheckerException('Assert must evaluate a boolean expression, not ' + ty.to_string())
-            assertexpr.ty = ty
-            return ty
+            expty = self.type_of(assertexpr, table)
+            if type(expty) is not BoolResolvedType:
+                raise TypeCheckerException('Assert must evaluate a boolean expression, not ' + expty.to_string())
+            assertexpr.ty = expty
+            return expty
         elif type(baseexpr) is TimeCmd:
             return self.type_of(baseexpr.cmd, table)
         elif type(baseexpr) is WriteCmd:
             writeexpr = baseexpr.expr
-            ty = self.type_of(writeexpr, table)
-            writeexpr.ty = ty
-            return ty
+            expty = self.type_of(writeexpr, table)
+            imgty = ArrayResolvedType(TupleResolvedType([FloatResolvedType(), FloatResolvedType(), FloatResolvedType(), FloatResolvedType()]), 2)
+            if not imgty.equals(expty):
+                ret = 'You cannot write a non {float, float, float, float}[,] to an image!'
+                raise TypeCheckerException(ret
+                                           )
+            writeexpr.ty = expty
+            return expty
         elif type(baseexpr) is LetCmd:
             letexpr = baseexpr.expr
-            ty = self.type_of(letexpr, table)
-            if type(baseexpr.lvalue) is TupleLValue:
-                self.addtuplelval(baseexpr.lvalue, table, ty)
-            else:
-                self.addlval(baseexpr.lvalue, table, ty)
-            letexpr.ty = ty
-            return ty
+            expty = self.type_of(letexpr, table)
+            # TODO add to symtbl
+            table.addlval(baseexpr.lvalue, expty)
+            letexpr.ty = expty
+            return expty
         elif type(baseexpr) is ReadCmd:
             imgty = ArrayResolvedType(TupleResolvedType([FloatResolvedType(), FloatResolvedType(), FloatResolvedType(), FloatResolvedType()]), 2)
-            table.addinfo(baseexpr.vararg.variable.variable, VariableInfo(imgty))
+            if type(baseexpr.vararg) is VarArg:
+                table.addinfo(baseexpr.vararg.variable.variable, VariableInfo(imgty))
+            elif type(baseexpr.vararg) is ArrayArgument:
+                if len(baseexpr.vararg.arguments) != 2:
+                    ret = 'Cannot bind image (type ' + imgty.to_string() + ') to ' + baseexpr.vararg.to_string() + ') (your array dimensions do not match!)'
+                    raise TypeCheckerException(ret)
+                for args in baseexpr.vararg.arguments:
+                    table.addinfo(args.variable, VariableInfo(IntResolvedType()))
+                table.addinfo(baseexpr.vararg.var.variable, VariableInfo(imgty))
+            else:
+                ret = 'Cannot bind image (type ' + imgty.to_string() + ') to ' + baseexpr.vararg.to_string()
+                raise TypeCheckerException(ret)
             return imgty
--- symboltable.py
+++ symboltable.py
@@ -47,6 +47,26 @@ class SymbolTable:
             raise TypeCheckerException('You cannot shadow ' + name)
         self.table[name] = info
 
+    def addarg(self, arg, typ):
+        if type(arg.variable) is VarArg:
+            name = arg.variable.variable.variable
+            self.addinfo(name, VariableInfo(typ))
+        elif type(arg.variable) is ArrayArgument:
+            name = arg.variable.var.variable
+            self.addinfo(name, VariableInfo(typ))
+            for args in arg.variable.arguments:
+                argname = args.variable
+                self.addinfo(argname, VariableInfo(IntResolvedType()))
+
+    def addlval(self, lval, typ):
+        if type(lval) is TupleLValue:
+            for i in range(len(lval.variables)):
+                self.addlval(lval.variables[i], typ.tys[i])
+        else:
+            self.addarg(lval, typ)
+
+
+
 
 
 
